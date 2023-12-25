5c4e7095a7414b076d8131d9346c1ddc067053aa
Wasthu-T <110516139+Wasthu-T@users.noreply.github.com>
2023-12-25 21:03:54 +0700

    todo.py

--- query/todo.py
+++ query/todo.py
@@ -9,7 +9,52 @@ import locale
 locale.setlocale(locale.LC_TIME, 'id_ID')
 
 class transaksi :
-    pass
+    def __init__(self, db):
+        self.db = db
+
+    def jenis_pembayaran(self):
+        while True :
+            print("\n=== Jenis Pembayaran ===")
+            print("1. Cash")
+            print("2. Transfer")
+            pilih = int(input("Pilih Jenis Pembayaran\t: "))
+            if pilih == 1 :
+                return "Cash"
+            elif pilih == 2 :
+                return "Transfer"
+            else :
+                print("pilihan tidak tersedia harap pilih yang benar")
+
+    def insert_transaksi(self):
+        print("===== Input Transaksi =====")
+        Id_pegawai = int(input("Masukan ID Pegawai\t: "))
+        Id_siswa = int(input("Masukan ID Siswa\t: "))
+        Id_paket_belajar = int(input("Masukan ID Paket Belajar\t: "))
+
+        Jenis_pembayaran = self.jenis_pembayaran()
+        Bayar = int(input("Masukan Nominal Bayar\t: "))
+        Kembalian = int(input("Masukan Jumlah Kembalian\t: "))
+        date_str = input("Masukkan tanggal transaksi (format: YYYY-MM-DD)\t: ") 
+        date = datetime.strptime(date_str, "%Y-%m-%d") 
+        Tgl_transaksi = date.date()
+        x = PrettyTable()
+        x.field_names = ["Id_pegawai", "Id_siswa","Id_paket_belajar", "Jenis_pembayaran", "Bayar", "Kembalian", "Tgl_transaksi"]
+        x.add_row([Id_pegawai, Id_siswa, Id_paket_belajar, Jenis_pembayaran, Bayar, Kembalian, Tgl_transaksi])
+        print(x)
+        yakin = str(input("Yakin ingin meng-input y/n? "))
+        if (yakin == 'y') :
+            quary = """INSERT INTO transaksi(Id_pegawai, Id_siswa, Id_paket_belajar, Jenis_pembayaran, Bayar, Kembalian, Tgl_transaksi) VALUES (%s, %s, %s, %s, %s, %s, %s)"""
+            data = (Id_pegawai, Id_siswa, Id_paket_belajar, Jenis_pembayaran, Bayar, Kembalian, Tgl_transaksi)
+
+            self.db.insertValue(quary,data)
+            print("=== Anda Berhasil Meng-input transaksi ===")
+        else : 
+            print("=== Anda Gagal Meng-input transaksi ===")
+
+    def read_transaksi(self):
+        print("=== Read Transaksi ===")
+        query = """SELECT * FROM transaksi"""
+        self.db.selectValuepretty(query, data=None)
 
 class jadwal : 
     pass
@@ -18,7 +63,98 @@ class jadwal_pelayanan :
     pass
 
 class ruangan :
-    pass
+    def __init__(self, db):
+        self.db = db
+
+    def kondisi_ruangan(self):
+        while True :
+            print("\n=== Kondisi Ruangan ===")
+            print("1. Layak")
+            print("2. Tidak layak")
+            pilih = int(input("Pilih Kondisi Ruangan\t: "))
+            if pilih == 1 :
+                return "Layak"
+            elif pilih == 2 :
+                return "Tidak Layak"
+            else :
+                print("pilihan tidak tersedia harap pilih yang benar")
+
+    def insert_ruangan(self):
+        print("===== Input Ruangan =====")
+        Kondisi_ruangan = self.kondisi_ruangan()
+        Kapasitas_kursi = int(input("Masukan Kapasitas Kursi\t: "))
+        x = PrettyTable()
+        x.field_names = ["Kondisi_ruangan", "Kapasitas_kursi"]
+        x.add_row([Kondisi_ruangan, Kapasitas_kursi])
+        print(x)
+        yakin = str(input("Yakin ingin meng-input y/n? "))
+        if (yakin == 'y') :
+            quary = """INSERT INTO ruangan(Kondisi_ruangan, Kapasitas_kursi) VALUES (%s, %s)"""
+            data = (Kondisi_ruangan. Kapasitas_kursi)
+
+            self.db.insertValue(quary,data)
+            print("=== Anda Berhasil Meng-input Data Ruangan ===")
+        else : 
+            print("=== Anda Gagal Meng-input Data Ruangan ===")
+
+    def edit_ruangan(self, result, Id_ruangan) :
+        Kondisi_ruangan = result[0][1]
+        Kapasitas_kursi = result[0][2]
+        while True :
+            print("=== Edit Value ===")
+            print("1. Kondisi Ruangan")
+            print("2. Kapasitas Kursi")
+            pilih = int(input("Data yang ingin diubah : "))
+            if pilih == 1:
+                Kondisi_ruangan = self.kondisi_ruangan()
+            elif pilih == 2:
+                Kapasitas_kursi = int(input("Masukan Kapasitas Kursi\t: "))
+            else : 
+                print("Pilihan tidak tersedia")
+            lanjut = str(input("Ganti data lain (y/n)? "))
+            if lanjut == 'y' :
+                continue
+            else :
+                break
+        query = """UPDATE ruangan SET `Kondisi_ruangan`= %s, `Kapasitas_kursi`=%s WHERE `Id_ruangan` = %s"""
+        data = (Kondisi_ruangan, Kapasitas_kursi)
+        self.db.insertValue(query, data)
+
+    def update_ruangan(self) :
+        print("=== Update Ruangan ===")
+        Id_ruangan = int(input("Masukkan ID Ruangan yang akan diupdate: "))
+        query = """SELECT * FROM ruangan WHERE Id_ruangan = %s"""
+        data = (Id_ruangan,)
+        self.db.selectValuepretty(query, data)
+        result = self.db.selectValue(query, data)
+        test = str(input("Apa data ingin diupdate (y/n)? "))
+        if test.lower() == 'y' :
+            self.edit_ruangan(result, Id_ruangan)
+            print("=== Anda Berhasil Meng-update Data Ruangan ===")
+        else :
+            print("=== Anda Gagal Meng-update Data Ruangan ===")
+
+    def delete_ruangan(self):
+        print("=== Delete Ruangan ===")
+        Id_ruangan = int(input("Masukkan ID Ruangan yang akan dihapus: "))
+        query = """SELECT * FROM ruangan WHERE Id_ruangan = %s"""
+        data = (Id_ruangan,)
+        self.db.selectValuepretty(query, data)
+        test = str(input("Apa data ingin dihapus (y/n)? "))
+        if test.lower() == 'y' :
+            query = """DELETE FROM ruangan WHERE Id_ruangan = %s """
+            data = (Id_ruangan,)
+            self.db.insertValue(query, data)
+            print("=== Anda Berhasil Menghapus Data Ruangan ===")
+            
+        else :
+            print("=== Anda Gagal Menghapus Data Ruangan ===")
+
+    def read_ruangan(self) :
+        print("=== Read Ruangan ===")
+        query = """SELECT * FROM ruangan"""
+        self.db.selectValuepretty(query, data=None)
+        print("=== Anda Berhasil Menampilkan Data Ruangan ===")
 
 class paket_belajar :
     def __init__(self, db):
