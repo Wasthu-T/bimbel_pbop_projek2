b4906be1fa303474dc73ca2a20741c40a693a25c
Wasthu-T <110516139+Wasthu-T@users.noreply.github.com>
2023-12-26 17:12:46 +0700

    Update akses.py

--- query/akses.py
+++ query/akses.py
@@ -15,9 +15,9 @@ class akses_Siswa(Siswa):
         while True :
             print(f"=== Selamat Datang {self.nama} ===")
             print("==[1] Absen ")
-            print("==[2] Cari ")
-            print("==[3] Insert ")
-            print("==[4] Update")
+            print("==[2] Lihat Jadwal")
+            print("==[3] Paket Belajar ")
+            print("==[4] Edit Akun")
             print("==[5] Detele")
             print("==[0] logout")
             pilih = int(input("Pilih menu : "))
@@ -40,44 +40,18 @@ class akses_Siswa(Siswa):
                 print("Pilihan tidak tersedia")
             os.system('pause')
 
-    def Absen_Murid(self) :
-            while True :
-                print(f"=== Selamat Datang {self.nama} ===")
-                print(f"=== Menu Absen ===")
-                print(f"=== [1] Lihat Absensi")
-                print(f"=== [2] Absen Jam datang")
-                print(f"=== [3] Absen Jam selesai")
-                print(f"=== [4] Absen Siswa")
-                print(f"=== [0] kembali")
-                pilih = int(input("Pilih Menu : "))
-                ag = absen_guru(db)
-                if pilih == 1 :
-                    ag.izin(self.id)
-                elif pilih == 2 :
-                    ag.absen_datang(self.id)
-                elif pilih == 3 :
-                    ag.absen_pulang(self.id)
-                elif pilih == 4 :
-                    ag.absen_siswa(self.id)
-                elif pilih == 0 :
-                    self.menu()
-                else :
-                    print("Pilihan tidak tersedia")
-                os.system('pause')
-
 class akses_Guru(Guru):
     def __init__(self, db, id, nama):
         super().__init__(db)
         self.id = id
         self.nama = nama
+        
     def menu(self) :
             while True :
                 print(f"=== Selamat Datang {self.nama} ===")
                 print("==[1] Absen ")
-                print("==[2] Cari ")
-                print("==[3] Insert ")
-                print("==[4] Update")
-                print("==[5] Detele")
+                print("==[2] Lihat Jadwal ")
+                print("==[3] Edit Akun")
                 print("==[0] logout")
                 pilih = int(input("Pilih menu : "))
                 if pilih == 1 :
@@ -133,22 +107,22 @@ class akses_Pegawai(Pegawai):
         while True :
             print(f"=== Selamat Datang {self.nama} ===")
             print("==[1] Absen ")
-            print("==[2] Cari ")
-            print("==[3] Insert ")
-            print("==[4] Update")
+            print("==[2] Cari Data")
+            print("==[3] Tambah Data")
+            print("==[4] Ubah Data")
             print("==[5] Detele")
             print("==[0] logout")
             pilih = int(input("Pilih menu : "))
             if pilih == 1 :
-                self.Absen_pegawai()
+                self.Absen_pegawai() # done
             elif pilih == 2 :
-                self.Cari()
+                self.Cari() #not yet test
             elif pilih == 3 :
-                self.Cari()
+                self.Insert()
             elif pilih == 4 :
-                self.Cari()
+                self.ubah()
             elif pilih == 5 :
-                self.Cari()
+                self.delete()
             elif pilih == 0 :
                 print(f"\n\t=== Terimakasih {self.nama} ===")
                 print("=== Jangan Lupa Datang Kembali ===\n")
@@ -182,7 +156,7 @@ class akses_Pegawai(Pegawai):
 
     def Cari(self) :
         while True :
-            print(f"=== selamat datang {self.nama} ===")
+            print(f"=== Selamat Datang {self.nama} ===")
             print(f"=== Menu Cari ===")
             print("=== [1] Pegawai")
             print("=== [2] Guru")
@@ -190,11 +164,13 @@ class akses_Pegawai(Pegawai):
             print("=== [0] kembali")
             pilih = int(input("Pilih menu : "))
             if pilih == 1 : 
-                pass
-            if pilih == 2 : 
-                pass
-            if pilih == 3 : 
-                pass
+                self.read_Pegawai()
+            elif pilih == 2 : 
+                gu = Guru(db)
+                gu.read_guru()
+            elif pilih == 3 : 
+                mu = Siswa(db)
+                mu.read_siswa()
             elif pilih == 0 :
                 self.menu()
             else :
@@ -204,8 +180,8 @@ class akses_Pegawai(Pegawai):
         
     def Insert(self) :
         while True :
-            print(f"=== selamat datang {self.nama} ===")
-            print(f"=== Menu Insert ===")
+            print(f"=== Selamat Datang {self.nama} ===")
+            print(f"=== Menu Tambah Data ===")
             print("===[1] Transaksi")
             print("===[2] Jadwal Pelajaran")
             print("===[3] Jadwal Pelayanan")
@@ -220,28 +196,33 @@ class akses_Pegawai(Pegawai):
             else :
                 print("Pilihan tidak tersedia")
             os.system('pause')
-    def tambah(self) :
+
+
+    def ubah(self) :
         while True :
             print(f"=== selamat datang {self.nama} ===")
             print(f"=== Menu Read ===")
-            print("===[1] kembali")
-            print("===[2] kembali")
-            print("===[3] kembali")
+            print("===[1] Ubah data diri")
+            print("===[2] Ubah data ruang")
+            print("===[3] Ubah Jadwal Pelajaran")
+            print("===[4] Ubah Jadwal Pelayanan")
+            print("===[5] Ubah Paket Belajar")
             print("===[0] kembali")
             pilih = int(input("Pilih menu : "))
             if pilih == 1 : 
-                pass
+                self.update_pegawai(self.id)
             elif pilih == 0 :
                 self.menu()
             else :
                 print("Pilihan tidak tersedia")
-    def tambah(self) :
+
+    def delete(self) :
         while True :
             print(f"=== selamat datang {self.nama} ===")
-            print(f"=== Menu Read ===")
-            print("===[1] kembali")
-            print("===[2] kembali")
-            print("===[3] kembali")
+            print(f"=== Menu Hapus ===")
+            print("===[1] Hapus data diri")
+            print("===[2] Hapus data guru")
+            print("===[3] Hapus data siswa")
             print("===[0] kembali")
             pilih = int(input("Pilih menu : "))
             if pilih == 1 : 
